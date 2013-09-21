435fbf5841a95486388f07bc87732931d74855e6
Kenan Yildirim <kenan@kenany.me>
2013-09-20 23:31:21 -0400

    Don't add weird blank trips

--- test/generate-trip-list.js
+++ test/generate-trip-list.js
@@ -5,11 +5,11 @@ var hyperquest = require('hyperquest');
 var write = concat(function(data) {
   var result = {};
   var lines = data.toString().split('\n');
-  forEach(function(lines) {
-    var pair = lines.split('!');
-    result[pair[0]] = pair[1];
+  lines.forEach(function(line) {
+    var pair = line.split('!');
+    if (pair[3]) result[pair[0]] = pair[1];
   });
-  fs.writeFileSync('./test/tripcodes.json', JSON.stringify(result));
+  fs.writeFileSync('tripcodes.json', JSON.stringify(result));
 });
 
 hyperquest('http://www.pageoftext.com/PH_plain&nm_page=secure_tripcode_dictionary').pipe(write);
