a1d2e575816c7902478cad20e7b2838ecb4d9990
Kenan Yildirim <kenan@kenany.me>
2013-12-13 23:29:57 -0500

    Add a few more tests for escaped characters

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var test = require('tape');
 
 var generateTripList = require('./generate-trip-list');
 
-test('basic characters', function(t) {
+test('\nbasic characters', function(t) {
   generateTripList(function(error, tripcodes) {
     if (error) throw error;
 
@@ -20,18 +20,30 @@ test('basic characters', function(t) {
   });
 });
 
-test('characters that are escaped', function(t) {
-  t.plan(1);
-  t.equal(tripcode('&<>"\''), 'CgqvGaJbDQ');
+test('\ncharacters that are escaped', function(t) {
+  var ESCAPE_TEST = [
+    ['&', 'MhCJJ7GVT.'],
+    ['<', 'D1YGKrvmeg'],
+    ['>', 'afqVxck0Ts'],
+    ['"', 'gt1azVccY2'],
+    // ['\'', '8/08awL.AE'],
+    ['&<>"\'', 'CgqvGaJbDQ']
+  ];
+
+  t.plan(ESCAPE_TEST.length);
+
+  forEach(ESCAPE_TEST, function(trip) {
+    t.equal(tripcode(trip[0]), trip[1]);
+  });
 });
 
 // 4chan strips utf8
-test('utf8 symbols', function(t) {
+test('\nutf8 symbols', function(t) {
   t.plan(1);
   t.equal(tripcode('©'), '');
 });
 
-test('collisions, oh my!', function(t) {
+test('\ncollisions, oh my!', function(t) {
   t.plan(1);
 
   // U+8A1B CJK UNIFIED IDEOGRAPH-8A1B
